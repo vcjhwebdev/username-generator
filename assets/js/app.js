06f23d7e21f1b054574c105638f0401521c0181a
Billy Roeschlein <william.roeschlein@warriorlife.net>
2019-01-31 09:20:56 -0800

    added document.write's
    app.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,6 @@ var x = document.getElementById("animalSelect").selectedIndex;
     alert(document.getElementsByTagName("option")[x].value);
     var z = document.getElementById("numberSelect").selectedIndex;
       alert(document.getElementsByTagName("option")[x].value);
+function myFunction() {
+  document.write(x + y + z)
+}
