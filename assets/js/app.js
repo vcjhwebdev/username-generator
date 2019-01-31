9c38028851c83c3231e59a0f3942640ae370f90b
Billy Roeschlein <william.roeschlein@warriorlife.net>
2019-01-31 09:18:20 -0800

    updated
    thingy with js

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var x = document.getElementById("animalSelect").selectedIndex;
+  alert(document.getElementsByTagName("option")[x].value);
+  var y = document.getElementById("hobbieSelect").selectedIndex;
+    alert(document.getElementsByTagName("option")[x].value);
+    var z = document.getElementById("numberSelect").selectedIndex;
+      alert(document.getElementsByTagName("option")[x].value);
