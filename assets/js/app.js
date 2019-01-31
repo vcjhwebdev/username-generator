50d01223fb7cec44d0446e23f6d217a1f7a1471d
Billy Roeschlein <william.roeschlein@warriorlife.net>
2019-01-31 09:52:11 -0800

    updated app.js
    tried to generate username, didn't work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
-var x = document.getElementById("animalSelect").selectedIndex;
-  alert(document.getElementsByTagName("option")[x].value);
-  var y = document.getElementById("hobbieSelect").selectedIndex;
-    alert(document.getElementsByTagName("option")[x].value);
-    var z = document.getElementById("numberSelect").selectedIndex;
-      alert(document.getElementsByTagName("option")[x].value);
 function myFunction() {
+    var x = document.getElementById("animalSelect").selectedIndex;
+      document.write(document.getElementsByTagName("option")[x].value);
+
+
+    var y = document.getElementById("hobbieSelect").selectedIndex;
+      document.write(document.getElementsByTagName("option")[y].value);
+
+
+    var z = document.getElementById("numberSelect").selectedIndex;
+      document.write(document.getElementsByTagName("option")[z].value);
+    }
+
+
+
   document.write(x + y + z)
-}
+
+/*
+
+function myFunction() {
+  var x = document.getElementById("numberSelect").selectedIndex;
+  alert(document.getElementsByTagName("option")[x].value);
+} */
