d912525da5891ab00c01d568c24e1301eceb56fe
Billy Roeschlein <william.roeschlein@warriorlife.net>
2019-02-12 09:03:25 -0800

    Added ShuffleArray

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,39 +4,37 @@ var form = document.getElementById('usernameForm');
 
 form.addEventListener("submit", function(event){
   event.preventDefault();
+  var x = document.getElementById("animalSelect").selectedIndex;
+  var animal = document.getElementsByTagName("option")[x].value;
+
+  var y = document.getElementById("hobbySelect").selectedIndex;
+  var hobby = document.getElementsByTagName("option")[y].value;
+
+  var z = document.getElementById("numberSelect").selectedIndex;
+  var number = document.getElementsByTagName("option")[z].value;
+
+  var output = document.querySelector('.output');
+
+  var words = [];
+  words.push(animal);
+  words.push(hobby);
+  words.push(number);
+
+  var shuffled = shuffleArray(words);
+
+  var username = shuffled.join();
+
+  output.textContent = username;
 });
 
-
-  function myFunction() {
-
-    var x = document.getElementById("animalSelect").selectedIndex;
-    var animal = document.getElementsByTagName("option")[x].value;
-
-
-    var y = document.getElementById("hobbySelect").selectedIndex;
-    var hobby = document.getElementsByTagName("option")[y].value;
+/**
+ * Takes an array and returns a shuffled array
+ */
+function shuffleArray(array) {
 
 
-    var z = document.getElementById("numberSelect").selectedIndex;
-    var number = document.getElementsByTagName("option")[z].value;
-
-    var output = document.querySelector('.output');
-
-    var words = [];
-    words.push(animal);
-    words.push(hobby);
-    words.push(number);
-    return words.join();
-
-    var result = shuffleWords(words);
-
-
-
-    output.textContent = result;
-    console.log(result);
-    }
-
-
+  return array;
+}
 
     //  var result = Math.random();
     // if(result < 0.33)  {
