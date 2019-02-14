cca3928ea63ee25921789f1c734029992ecaeb5c
Billy Roeschlein <william.roeschlein@warriorlife.net>
2019-02-14 09:34:10 -0800

    shuffled it so it randomizes ONE word.

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,8 +31,17 @@ form.addEventListener("submit", function(event){
  * Takes an array and returns a shuffled array
  */
 function shuffleArray(array) {
+  var i = 0
+     , j = 0
+     , temp = null
 
-
+   for (i = array.length - 1; i > 0; i -= 1) {
+     j = Math.floor(Math.random() * (i + 1))
+     temp = array[i]
+     array[i] = array[j]
+     array[j] = temp
+   }
+return array;
 }
 
 
