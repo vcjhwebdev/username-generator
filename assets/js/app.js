ad9f331b6c8d5514678f59ea1eb8ac7ac05f353d
Billy Roeschlein <william.roeschlein@warriorlife.net>
2019-04-01 09:13:00 -0700

    Update app.js
    reverted

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,24 +66,3 @@ function local()	{
 
 // var test = 1
 // localStorage.setItem(Math.random(), test);
-// dots is an array of Dot objects,
-// mouse is an object used to track the X and Y position
-   // of the mouse, set with a mousemove event listener below
-   window.addEventListener("mousemove", function (e) {
-   	var to_append = document.getElementsByClassName('loader-container')[0];
-   	var all = document.getElementsByClassName('loader-container');
-
-   	var parent_div = document.createElement('div');
-   	parent_div.className = "loader-container";
-   	var inner_div = document.createElement('div');
-   	inner_div.className = "loader";
-   	parent_div.appendChild(inner_div)
-   	var d = document.body.appendChild(parent_div);
-
-   	parent_div.style.left = (e.clientX - 50)+'px';
-   	parent_div.style.top = (e.clientY - 50)+'px';
-
-   	if(document.getElementsByClassName('loader-container').length > 50) {
-   		document.body.removeChild(to_append)
-   	}
-   });
